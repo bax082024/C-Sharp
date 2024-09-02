9af9d09f6089d3c911e42da68d980cbdd35e664a
bax082024 <bax082024@gmail.com>
2024-09-02 10:56:09 +0200

    c#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace C_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string helloWorld = "hello , world";
+        
+        int myNumber = 10;
+
+        double myDecimal = 10.25;
+        
+        Console.WriteLine(helloWorld);
+        Console.WriteLine(myNumber);
+        Console.WriteLine(myDecimal);
+        // skriver ut en melding
+        Console.WriteLine("Hei, hva heter du?");
+        //string variabel, nav holder på verdien vi gir.
+        string? navn = Console.ReadLine();
+        Console.WriteLine("Hei"  + navn);
+
+        //char typen, tar en bestemt bokstav
+        char a = '@';
+        
+        Console.WriteLine(a);
+
+        
+    
+
+    }
+}
