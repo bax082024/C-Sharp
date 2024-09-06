a3234d32324b3aa5ef85f0aef50509b4f5e8abb4
bax082024 <bax082024@gmail.com>
2024-09-06 10:08:02 +0200

    added oppgave

--- C# øve/Program.cs
+++ C# øve/Program.cs
@@ -4,8 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("I like pizza");
-        
+
+      /*  for (int i = 1; i <= 10; i+= 2)
+        {
+            Console.WriteLine(i);
+        }
+      */
+        Human Human1 = new Human();
+        Human Human2 = new Human();
+
+        Human1.name = "Rick ";
+        Human1.age = 65;
+
+        Human2.name = "Leonard ";
+        Human2.age = 28;
+
+        Human1.Eat();
+        Human1.Sleep();
+
+        Human2.Eat();
+        Human2.Sleep();
+
+        Console.ReadKey();
     }
+}
+
+ class Human
+{
+    public String name;
+    public int age;
+
+    public void Eat() 
+    {
+        Console.WriteLine(name + "Is Eating");
+    }
+
+    public void Sleep() 
+    {
+        Console.WriteLine(name + "Is Sleeping");
+
+    }
+
+
+
+
+
+
+
+
 }
