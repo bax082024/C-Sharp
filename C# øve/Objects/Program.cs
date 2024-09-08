b53f4ff536c5b313d055de08dedaa38f114d9744
bax082024 <bax082024@gmail.com>
2024-09-08 19:49:09 +0200

    Added TicTacToe

---
+++ C# øve/Objects/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Objects;
+
+class Program
+{
+    /* Oppgave 2 How To Call a Method
+    static void MyMethod() 
+    {
+        Console.WriteLine("I just got executed");
+    }
+    */
+    static void Main(string[] args)
+    {
+        /* Oppgave 1 List of players----------
+        List<Player> players = new List<Player>();
+
+        Player player1 = new Player("John");
+        Player player2 = new Player("Mason");
+        Player player3 = new Player("Michael");
+
+        players.Add(new Player("John"));
+        players.Add(new Player("Mason"));
+        players.Add(new Player("Michael"));
+
+        foreach(Player player in players) 
+        {
+            Console.WriteLine(player);
+        }
+       ---------------------- */
+
+        // Oppgave 2
+        // MyMethod();
+
+     
+
+    }
+}
+
+
+/* Oppgave 1 class Player
+{
+
+    public String username;
+
+    public Player(String username) 
+    {
+        this.username = username;
+    }
+
+    public override string ToString()
+    {
+        return username;
+    }
+
+}
+*/
