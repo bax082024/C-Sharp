5cd24eec6762992546ebd52fac2b75d6764606d7
bax082024 <bax082024@gmail.com>
2024-09-16 15:41:06 +0200

    Added Oppgave i C#

---
+++ Oppgaver C# Klasser, Interfaces og metoder/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Oppgaver_C__Klasser__Interfaces_og_metoder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+        
+
+    }
+    
+}
